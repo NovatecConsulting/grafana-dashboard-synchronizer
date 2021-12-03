b71e4b36a21dc8a7b287acc4801a55b7a13b6622
Marius Oehler <marius.oehler@novatec-gmbh.de>
2021-12-03 07:41:44 +0100

    Using InfoBox for query editor

--- src/QueryEditor.tsx
+++ src/QueryEditor.tsx
@@ -2,16 +2,21 @@ import React, { PureComponent } from 'react';
 import { QueryEditorProps } from '@grafana/data';
 import { DataSource } from './datasource';
 import { MyDataSourceOptions, MyQuery } from './types';
-import { Label } from '@grafana/ui';
+import { InfoBox } from '@grafana/ui';
 
 type Props = QueryEditorProps<DataSource, MyQuery, MyDataSourceOptions>;
 
 export class QueryEditor extends PureComponent<Props> {
   render() {
     return (
-      <Label description="This data source is not intended to provide data or for use in dashboard panels.">
-        Do not use this data source!
-      </Label>
+      <InfoBox
+        title="Do Not Use This Data Source!"
+        severity="error"
+        url="https://github.com/NovatecConsulting/grafana-dashboard-sync-plugin"
+      >
+        This data source is <strong>not</strong> intended to be used to query data or to be used in dashboards and
+        panels.
+      </InfoBox>
     );
   }
 }
