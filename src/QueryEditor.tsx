1443c0ab268f9be81c7c8d7534c4fe2f9dd650fe
Marius Oehler <mariusoe@users.noreply.github.com>
2021-12-03 08:29:10 +0100

    Finalizing the Query Editor UI (#14)

--- src/QueryEditor.tsx
+++ src/QueryEditor.tsx
@@ -2,16 +2,21 @@ import React, { PureComponent } from 'react';
 import { QueryEditorProps } from '@grafana/data';
 import { DataSource } from './datasource';
 import { MyDataSourceOptions, MyQuery } from './types';
-import { Label } from '@grafana/ui';
+import { InfoBox } from '@grafana/ui';
 
 type Props = QueryEditorProps<DataSource, MyQuery, MyDataSourceOptions>;
 
 export class QueryEditor extends PureComponent<Props> {
   render() {
     return (
-      <div className="gf-form">
-        <Label description="Only for Backend">Grafana Git Workflow</Label>
-      </div>
+      <InfoBox
+        title="Do Not Use This Data Source!"
+        severity="error"
+        url="https://github.com/NovatecConsulting/grafana-dashboard-sync-plugin"
+      >
+        This data source is <strong>not</strong> intended to be used to query data or to be used in dashboards and
+        panels.
+      </InfoBox>
     );
   }
 }
